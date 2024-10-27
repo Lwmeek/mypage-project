17586d98c30abfe3692e8107b3d6c70c3b92cc37
Timic Ivey <timicivey27@gmail.com>
2024-10-27 16:28:47 -0400

    starting project with radix ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css"
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Theme accentColor="ruby">
+			<App />
+		</Theme>
+	</StrictMode>
+);
