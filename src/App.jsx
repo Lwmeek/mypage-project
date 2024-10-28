3d985d8ff8d8c53d1826d6be8458b6832e5d679e
Timic Ivey <timicivey27@gmail.com>
2024-10-27 20:27:50 -0400

    playing with project to get back into it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import { Box, Container } from "@radix-ui/themes";
+import { Box, Container, Section, Flex } from "@radix-ui/themes";
+import { ShadowIcon } from "@radix-ui/react-icons";
 
 function App() {
 	return (
 		<>
 			<Box style={{ background: "gray" }} height="100vh">
-				<Container m={"1"} align={"center"}>
+				<Section
+					mx="4"
+					py="1"
+					style={{
+						backgroundColor: "transparent",
+						color: "whitesmoke",
+						borderBottom: "solid 1px white",
+					}}
+				>
+					<Flex align="center" gap="9" justify="start">
+						<ShadowIcon />
+						<Flex gap="4">
+							<p>Home</p>
+							<p>About</p>
+							<p>Contact</p>
+							<p>Connect</p>
+						</Flex>
+						<img
+							src="src/images/sam.jpg"
+							alt="Samurai shadow"
+							className="samurai"
+						/>
+					</Flex>
+				</Section>
+				<Container>
 					<p>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
 						rem, nostrum accusantium saepe omnis tempore soluta dolor doloribus
